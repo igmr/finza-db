0ff8ce85307922eae4987084431e6aaea93f086d
Ivan Martinez <igmrivan@gmail.com>
2023-10-21 11:49:07 -0600

    Updated Foreign key

--- assets/script.sql
+++ assets/script.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users
 (
     id          INT           NOT NULL AUTO_INCREMENT   COMMENT "Identificador único para la administración de los usuarios.",
-    name        VARCHAR(255)      NULL DEFAULT 1        COMMENT "Nombre propio del usuario.",
+    name        VARCHAR(255)      NULL DEFAULT null     COMMENT "Nombre propio del usuario.",
     email       VARCHAR(255)      NULL DEFAULT NULL     COMMENT "Correo electrónico del usuario.",
     password    VARCHAR(255)      NULL DEFAULT NULL     COMMENT "Contraseña de acceso del usuario.",
     observation TEXT              NULL DEFAULT NULL     COMMENT "Opcional, observaciones del usuario.",
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS users
     updated_at  TIMESTAMP         NULL DEFAULT NOW()    COMMENT "Datos de auditoria, Fecha de actualización del registro.",
     deleted_at  TIMESTAMP         NULL DEFAULT NULL     COMMENT "Datos de auditoria, Fecha de baja del registro.",
     CONSTRAINT  pkUser            PRIMARY KEY(id),
-    CONSTRAINT  ukUser            UNIQUE(email, username)
+    CONSTRAINT  ukUser            UNIQUE(email)
 ) COMMENT "Catalogo de usuarios con privilegios para el acceso al sistema.";
 
 DROP TABLE IF EXISTS banks;
 CREATE TABLE IF NOT EXISTS banks
 (
     id           INT          NOT NULL  AUTO_INCREMENT   COMMENT "Identificador único para el catalogo de bancos.",
-    usr_id       INT              NULL  DEFAULT 1        COMMENT "Identificador del usuario.",
+    usr_id       INT              NULL  DEFAULT NULL     COMMENT "Identificador del usuario.",
     abbreviature CHAR(25)         NULL  DEFAULT NULL     COMMENT "Abreviatura del banco.",
     name         VARCHAR(65)      NULL  DEFAULT NULL     COMMENT "Nombre del banco.",
     observation  TEXT             NULL  DEFAULT NULL     COMMENT "Opcional, observaciones generales del banco.",
@@ -34,14 +34,16 @@ CREATE TABLE IF NOT EXISTS banks
     deleted_at   TIMESTAMP        NULL  DEFAULT now()    COMMENT "Datos de auditoria, fecha de baja del registro",
     CONSTRAINT   pkBank           PRIMARY KEY(id),
     CONSTRAINT   ukBank           UNIQUE(name),
-    CONSTRAINT   fkBankUser       FOREIGN KEY(usr_id) REFERENCES users(usr_id)
+    CONSTRAINT   fkBankUser       FOREIGN KEY(usr_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL
 ) COMMENT "Catalogo de bancos.";
 
 DROP TABLE IF EXISTS categories;
 CREATE TABLE IF NOT EXISTS categories
 (
     id           INT            NOT NULL AUTO_INCREMENT   COMMENT "Identificador único para el catalogo de categorías.",
-    usr_id       INT                NULL DEFAULT 1        COMMENT "Identificador del usuario.",
+    usr_id       INT                NULL DEFAULT NULL     COMMENT "Identificador del usuario.",
     code         CHAR(10)           NULL DEFAULT NULL     COMMENT "Opcional, código de la categoría.",
     name         VARCHAR(255)       NULL DEFAULT NULL     COMMENT "Descripción de la categoría.",
     icon         VARCHAR(15)        NULL DEFAULT NULL     COMMENT "Opcional, icono de categoría.",
@@ -54,7 +56,9 @@ CREATE TABLE IF NOT EXISTS categories
     deleted_at   TIMESTAMP          NULL DEFAULT NULL     COMMENT "Datos de auditoria, fecha de baja del registro.",
     CONSTRAINT   pkCategory         PRIMARY KEY(id),
     CONSTRAINT   ukCategory         UNIQUE(name),
-    CONSTRAINT   fkCategoryUser     FOREIGN KEY(usr_id) REFERENCES users(usr_id)
+    CONSTRAINT   fkCategoryUser     FOREIGN KEY(usr_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL
 ) COMMENT "Lista de categorías para clasificar los tipos";
 
 DROP TABLE IF EXISTS debts;
@@ -62,10 +66,10 @@ CREATE TABLE IF NOT EXISTS debts
 (
     id            INT         NOT NULL AUTO_INCREMENT  COMMENT "Identificador único para la administración de mis deudas.",
     usr_id        INT             NULL DEFAULT 1       COMMENT "Identificador del usuario.",
-    cat_id        INT             NULL DEFAULT NULL    COMMENT "Identificador de categoria.",
+    cat_id        INT             NULL DEFAULT 1       COMMENT "Identificador de categoria.",
     name          VARCHAR(65)     NULL DEFAULT NULL    COMMENT "Nombre de la deuda.",
     amount        DOUBLE          NULL DEFAULT 0       COMMENT "Importe total de la deuda.",
-    period        CHAR(15)        NULL DEFAULT NULL    COMMENT "Tipo de deuda: [diario|quincenal|mensual|anual].",
+    period        CHAR(15)        NULL DEFAULT NULL    COMMENT "Tipo de presupuesto: [diario|quincenal|mensual|anual].",
     day           INT             NULL DEFAULT NULL    COMMENT "Dia de pago de deuda para tipo de perido Mensual",
     date_at       DATETIME        NULL DEFAULT NULL    COMMENT "Fecha de pago de deuda para tipo de perido Anual",
     observation   TEXT            NULL DEFAULT NULL    COMMENT "Opcional, observaciones de la deuda.",
@@ -76,8 +80,9 @@ CREATE TABLE IF NOT EXISTS debts
     deleted_at    TIMESTAMP       NULL DEFAULT NULL    COMMENT "Datos de auditoria, fecha de baja del registro.",
     CONSTRAINT    pkDebt          PRIMARY KEY(id),
     CONSTRAINT    ukDebt          UNIQUE(name),
-    CONSTRAINT    fkDebtUser      FOREIGN KEY(usr_id) REFERENCES users(usr_id),
-    CONSTRAINT    fkDebtCategory  FOREIGN KEY(cat_id) REFERENCES categories(cat_id)
+    CONSTRAINT    fkDebtUser      FOREIGN KEY(usr_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL
 ) COMMENT "Administración de deudas.";
 
 DROP TABLE IF EXISTS savings;
@@ -95,7 +100,9 @@ CREATE TABLE IF NOT EXISTS savings
     deleted_at    TIMESTAMP       NULL DEFAULT NULL    COMMENT "Datos de auditoria, fecha de bajo del registro.",
     CONSTRAINT    pkSaving        PRIMARY KEY(id),
     CONSTRAINT    ukSaving        UNIQUE(name),
-    CONSTRAINT    fkSavingUser    FOREIGN KEY(usr_id) REFERENCES users(usr_id)
+    CONSTRAINT    fkSavingUser    FOREIGN KEY(usr_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL
 ) COMMENT "Administración de ahorros.";
 
 DROP TABLE IF EXISTS accounts;
@@ -112,8 +119,12 @@ CREATE TABLE IF NOT EXISTS accounts
     updated_at   TIMESTAMP         NULL DEFAULT NOW()  COMMENT "Datos de auditoria, fecha de actualización del registro.",
     deleted_at   TIMESTAMP         NULL DEFAULT NULL   COMMENT "Datos de auditoria, fecha de baja del registro.",
     CONSTRAINT   pkAccount         PRIMARY KEY(id),
-    CONSTRAINT   fkAccountUser     FOREIGN KEY(usr_id) REFERENCES users(usr_id),
-    CONSTRAINT   fkAccountBank     FOREIGN KEY(ban_id) REFERENCES banks(ban_id)
+    CONSTRAINT   fkAccountUser     FOREIGN KEY(usr_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkAccountBank     FOREIGN KEY(ban_id) REFERENCES banks(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL
 ) COMMENT "Catalogo de cuentas bancarias.";
 
 DROP TABLE IF EXISTS budgets;
@@ -133,8 +144,12 @@ CREATE TABLE IF NOT EXISTS budgets
     deleted_at   TIMESTAMP         NULL DEFAULT NULL   COMMENT "Datos de auditoria, fecha de baja del registro.",
     CONSTRAINT   pkBudget          PRIMARY KEY(id),
     CONSTRAINT   ukBudget          UNIQUE(name),
-    CONSTRAINT   fkBudgetUser      FOREIGN KEY(usr_id) REFERENCES users(usr_id),
-    CONSTRAINT   fkBudgetAccount   FOREIGN KEY(acc_id) REFERENCES accounts(acc_id)
+    CONSTRAINT   fkBudgetUser      FOREIGN KEY(usr_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkBudgetAccount   FOREIGN KEY(acc_id) REFERENCES accounts(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL
 ) COMMENT "Administración de presupuestos.";
 
 DROP TABLE IF EXISTS ingresses;
@@ -154,11 +169,21 @@ CREATE TABLE IF NOT EXISTS ingresses
     created_at   TIMESTAMP         NULL DEFAULT NOW()  COMMENT "Datos de auditoria, fecha de alta del registro.",
     updated_at   TIMESTAMP         NULL DEFAULT NOW()  COMMENT "Datos de auditoria, fecha de actualización del registro.",
     CONSTRAINT   pkIngress         PRIMARY KEY(id),
-    CONSTRAINT   fkIngressUser     FOREIGN KEY(usr_id) REFERENCES users(usr_id),
-    CONSTRAINT   fkIngressCategory FOREIGN KEY(cat_id) REFERENCES categories(cta_id),
-    CONSTRAINT   fkIngressSaving   FOREIGN KEY(sav_id) REFERENCES savings(sav_id),
-    CONSTRAINT   fkIngressDebt     FOREIGN KEY(deb_id) REFERENCES debts(deb_id),
-    CONSTRAINT   fkIngressAccount  FOREIGN KEY(acc_id) REFERENCES accounts(acc_id)
+    CONSTRAINT   fkIngressUser     FOREIGN KEY(usr_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkIngressCategory FOREIGN KEY(cat_id) REFERENCES categories(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkIngressSaving   FOREIGN KEY(sav_id) REFERENCES savings(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkIngressDebt     FOREIGN KEY(deb_id) REFERENCES debts(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkIngressAccount  FOREIGN KEY(acc_id) REFERENCES accounts(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL
 ) COMMENT "Operaciones de ingresos";
 
 DROP TABLE IF EXISTS egresses;
@@ -178,11 +203,21 @@ CREATE TABLE IF NOT EXISTS egresses
     created_at   TIMESTAMP         NULL DEFAULT NOW()  COMMENT "Datos de auditoria, fecha de alta del registro.",
     updated_at   TIMESTAMP         NULL DEFAULT NOW()  COMMENT "Datos de auditoria, fecha de actualización del registro.",
     CONSTRAINT   pkEgress          PRIMARY KEY(id),
-    CONSTRAINT   fkEgressUser      FOREIGN KEY(usr_id) REFERENCES users(usr_id),
-    CONSTRAINT   fkEgressCategory  FOREIGN KEY(cat_id) REFERENCES categories(cta_id),
-    CONSTRAINT   fkEgressSaving    FOREIGN KEY(sav_id) REFERENCES savings(sav_id),
-    CONSTRAINT   fkEgressDebt      FOREIGN KEY(deb_id) REFERENCES debts(deb_id),
-    CONSTRAINT   fkEgressAccount   FOREIGN KEY(acc_id) REFERENCES accounts(acc_id)
+    CONSTRAINT   fkEgressUser      FOREIGN KEY(usr_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkEgressCategory  FOREIGN KEY(cat_id) REFERENCES categories(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkEgressSaving    FOREIGN KEY(sav_id) REFERENCES savings(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkEgressDebt      FOREIGN KEY(deb_id) REFERENCES debts(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL,
+    CONSTRAINT   fkEgressAccount   FOREIGN KEY(acc_id) REFERENCES accounts(id)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL
 ) COMMENT "Operaciones de ingresos";
 
 DROP TABLE IF EXISTS transactions;
@@ -201,5 +236,5 @@ CREATE TABLE IF NOT EXISTS transactions
     file         VARCHAR(150)      NULL DEFAULT NULL   COMMENT "Opcional, nombre de archivo.",
     created_at   TIMESTAMP         NULL DEFAULT NOW()  COMMENT "Datos de auditoria, fecha de alta del registro.",
     updated_at   TIMESTAMP         NULL DEFAULT NOW()  COMMENT "Datos de auditoria, fecha de actualización del registro.",
-    CONSTRAINT   pkTransaction     PRIMARY KEY(tra_id)
+    CONSTRAINT   pkTransaction     PRIMARY KEY(id)
 ) COMMENT "Historial de operaciones de ingresos y egresos";
